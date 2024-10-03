eecc11d6bded9c7de7201a1708ccfa2c373c0cf3
Millat Khandakar <109697680+Millatkhandokar97@users.noreply.github.com>
2024-10-03 17:54:46 +0600

    Create short-hand.js

---
+++ short-hand.js
@@ -0,0 +1,23 @@
+let name = ''
+
+let fullname = name || 'MK Millat'
+console.log(fullname);
+
+
+let nam = 'MD Murad'
+
+let fullnam = nam || 'MK Millat'
+console.log(fullnam);
+
+
+
+
+let IsOk = true
+
+IsOk && console.log('Everything is Ok');
+
+let isOk = false
+
+isOk && console.log('Everything is Ok');
+
+isOk || console.log('There is found One error');
